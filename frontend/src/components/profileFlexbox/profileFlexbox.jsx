811078412c8013c9b2708ebc6bec96214d2908e8
ShahriarAlvi <alam.shahriaralvi@gmail.com>
2023-11-18 06:05:22 +0600

    flexbox updated

--- frontend/src/components/profileFlexbox/profileFlexbox.jsx
+++ frontend/src/components/profileFlexbox/profileFlexbox.jsx
@@ -37,7 +37,7 @@
 //               <ul>
 //                 {skills.map((skill) => (
 //                   <li key={skill}>
-//                     {skill} 
+//                     {skill}
 //                     console.log("Skill:", skill);
 //                   </li>
 //                 ))}
@@ -67,13 +67,16 @@ import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 
 export const ProfileFlexbox = () => {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ["profile_flexboxs"],
-    queryFn: () =>
-      makeRequest.get("/profile_flexboxs").then((res) => {
-        return res.data;
-      }),
-  }, console.log(1));
+  const { isLoading, error, data } = useQuery(
+    {
+      queryKey: ["profile_flexboxs"],
+      queryFn: () =>
+        makeRequest.get("/profile_flexboxs").then((res) => {
+          return res.data;
+        }),
+    },
+    console.log(1)
+  );
 
   console.log("klasjd " + data);
 
@@ -89,30 +92,42 @@ export const ProfileFlexbox = () => {
 
   data.forEach((item) => {
     const userID = item.userID;
-  
+
     if (!userData[userID]) {
       userData[userID] = []; // Initialize array for user ID
     }
-  
+
     userData[userID].push(item.skillName); // Pushing skill data
   });
-  
 
-  Object.keys(userData).forEach((userID) => {
-    const skillData = userData[userID];
+return (
+  <div className="profile-flexbox">
+    {data &&
+      Object.values(
+        data.reduce((acc, curr) => {
+          if (!acc[curr.userId]) {
+            acc[curr.userId] = [];
+          }
+          acc[curr.userId].push(curr.skill_name);
+          return acc;
+        }, {})
+      ).map((skills, userIdx) => (
+        <div key={userIdx} className="flexbox-container">
+          <h2>User ID: {userIdx}</h2>
+          <div className="skills">
+            {skills.map((skill, skillIdx) => (
+              <h4 key={skillIdx}>{skill}</h4>
+            ))}
+          </div>
+          <div className="buttons">
+            <button className="profile-detail-btn">Profile</button>
+            <button className="follow">Follow</button>
+          </div>
+        </div>
+      ))}
+  </div>
+);
 
-    return (
-      <div key={userID}>
-        <h3>User ID: {userID}</h3>
-        <p>Skills:</p>
-        <ul>
-          {skillData.map((skill) => (
-            <li key={skill}>{skill}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  });
 };
 
 export default ProfileFlexbox;
